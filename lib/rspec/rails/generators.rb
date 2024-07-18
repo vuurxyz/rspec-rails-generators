76ba600a7d1a91db1cf10e4e67744fd30d11c58d
lonnv <hey@vuur.xyz>
2024-07-18 12:18:13 +0200

    Initial setup

---
+++ lib/rspec/rails/generators.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "generators/version"
+
+module Rspec
+  module Rails
+    module Generators
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
